2b13456377595c4dcbf79a101d62cedf6ba93128
Roman <rosipenko@yandex.ru>
2022-07-12 20:25:28 +0500

    make

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -109,7 +109,7 @@ export const create = async (req, res) => {
     const doc = new PostModel({
       title: req.body.title,
       text: req.body.text,
-      tags: req.body.tags.split(','),
+      tags: req.body.tags.split(', '),
       imageUrl: req.body.imageUrl,
       user: req.userId,
     });
